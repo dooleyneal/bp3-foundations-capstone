60f2f8125dd9f501828f0f4c0a321100a520f8bb
Neal Dooley <nealdooley19@gmail.com>
2022-08-11 14:05:07 -0500

    deployment

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
-const {CONNECTION_STRING} = process.env
+const {DATABASE_URL} = process.env
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize(CONNECTION_STRING, {
+const sequelize = new Sequelize(DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
         ssl: {
